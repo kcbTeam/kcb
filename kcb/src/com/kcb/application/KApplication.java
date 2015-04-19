cb7a5a0a21e064d980d138294e50fc491d6b70b7
wanghang <847972062@qq.com>
2015-04-19 23:06:41 +0800

    test user nama and pasword save

--- kcb/src/com/kcb/application/KApplication.java
+++ kcb/src/com/kcb/application/KApplication.java
@@ -3,7 +3,6 @@ package com.kcb.application;
 import android.app.Application;
 
 public class KApplication extends Application {
-    private static final String TAG = "KApplication";
 
     @Override
     public void onCreate() {
