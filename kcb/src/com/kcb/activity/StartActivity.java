768e48b96bc2875f11486c14cabd786358fa8f4f
wanghang <847972062@qq.com>
2015-04-19 22:58:24 +0800

    add some package, add .gitignore, add two activity

---
+++ kcb/src/com/kcb/activity/StartActivity.java
@@ -0,0 +1,14 @@
+package com.kcb.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.kcb.R;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
---
+++ kcb/src/com/kcb/application/KApplication.java
@@ -0,0 +1,12 @@
+package com.kcb.application;
+
+import android.app.Application;
+
+public class KApplication extends Application {
+    private static final String TAG = "KApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
