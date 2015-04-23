5501034f02c219b060afd908df4ecaad94fd89f0
snowplayfire <493686801@qq.com>
2015-04-23 22:55:24 +0800

    teacher register page

---
+++ kcb/src/com/kcb/teacher/activity/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.kcb.teacher.activity;
+
+import com.kcbTeam.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+/**
+ * 
+ * @className: RegisterActivity
+ * @description:
+ * @author: LJX
+ * @date: 
+ */
+public class RegisterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.teacher_activity_register);
+	}
+
+
+}
