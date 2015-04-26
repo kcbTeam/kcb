e2d5d5304dd454c56b06d785a2607f56f6b695bc
ding1ding <1498773121@qq.com>
2015-04-26 21:18:00 +0800

    change fragment

---
+++ kcb/src/com/kcb/student/activity/SignInResultActivity.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright© 2011-2014 DewMobile USA Inc.All Rights Reserved.
+ * @Title: 	SignInResult.java 
+ * @Package com.kcb.student.activity 
+ * @Description: 
+ * @author:	Ding 
+ * @date:	2015年4月26日 下午8:45:09 
+ * @version	V1.0   
+ */
+
+package com.kcb.student.activity;
+
+/**
+ * @className: SignInResult
+ * @description: 
+ * @author: Ding
+ * @date: 2015年4月26日 下午8:45:09
+ */
+public class SignInResultActivity {
+    
+}
