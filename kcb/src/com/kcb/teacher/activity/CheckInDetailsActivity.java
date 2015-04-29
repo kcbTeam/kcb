4c8de9e2b097cb9d0de539adfb76614d77599d80
hitYOUcry <285692817@qq.com>
2015-04-29 17:19:50 +0800

    complete StuDetailsActivity and modify piechart animate

--- kcb/src/com/kcb/teacher/activity/CheckInDetailsActivity.java
+++ kcb/src/com/kcb/teacher/activity/CheckInDetailsActivity.java
@@ -6,13 +6,13 @@ import java.util.List;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
@@ -45,11 +45,14 @@ public class CheckInDetailsActivity extends BaseFragmentActivity
     private ListView missCheckInListView;
 
     private String mHintFormat = "本次到课率为%d%%";
-    private int mCheckInRate = 70;
+    private float mCheckInRate = 0.7f;
 
-    private String[] mParties = new String[] {"已签", "未签"};
+    private String[] mInfoStrings = new String[] {"已签", "未签"};
     private List<StudentInfo> mList;
     private ListAdapterMissCheckIn mAdapter;
+
+    private final int mDealy = 150;
+    private final int mAnimateDuration = 1800;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,7 +66,7 @@ public class CheckInDetailsActivity extends BaseFragmentActivity
     protected void initView() {
         initPieChart();
         hintTextView = (TextView) findViewById(R.id.textview_attendance_rate_hint);
-        hintTextView.setText(String.format(mHintFormat, mCheckInRate));
+        hintTextView.setText(String.format(mHintFormat, (int) (100 * mCheckInRate)));
         backButton = (Button) findViewById(R.id.button_back);
         backButton.setOnClickListener(new OnClickListener() {
 
@@ -102,90 +105,59 @@ public class CheckInDetailsActivity extends BaseFragmentActivity
 
     private void initPieChart() {
         mChart = (PieChart) findViewById(R.id.piechart);
-        mChart.setUsePercentValues(true);
-        mChart.setDescription("");
-        mChart.setDrawHoleEnabled(true);
-        mChart.setHoleColorTransparent(true);
+        setData(mChart, mInfoStrings);
+        setDefaultPieChartStyle(mChart);
+        new Handler().postDelayed(new Runnable() {
 
-        mChart.setTransparentCircleColor(Color.WHITE);
-
-        mChart.setHoleRadius(1f);
-        mChart.setTransparentCircleRadius(2f);
-        mChart.setHoleColor(Color.WHITE);
-
-        mChart.setDrawCenterText(true);
-
-        mChart.setRotationAngle(0);
-        // enable rotation of the chart by touch
-        mChart.setRotationEnabled(true);
-        setData(1, mCheckInRate);
-        mChart.animateY(1500, Easing.EasingOption.EaseInOutQuad);
-        // mChart.spin(2000, 0, 360);
-        //
-        Legend l = mChart.getLegend();
-        l.setEnabled(false);
-        // l.setPosition(LegendPosition.PIECHART_CENTER);
-        // l.setXEntrySpace(7f);
-        // l.setYEntrySpace(5f);
-
+            @Override
+            public void run() {
+                mChart.animateY(mAnimateDuration);
+                mChart.setVisibility(View.VISIBLE);
+            }
+        }, mDealy);
     }
 
-    private void setData(int count, float range) {
-
-        float mult = range;
+    private void setData(PieChart pieChart, String[] infoStrings) {
 
         ArrayList<Entry> yVals1 = new ArrayList<Entry>();
-        yVals1.add(new Entry(mult / 100, 0));
-        yVals1.add(new Entry((float) (1 - mult / 100), 1));
-        // IMPORTANT: In a PieChart, no values (Entry) should have the same
-        // xIndex (even if from different DataSets), since no values can be
-        // drawn above each other.
-        // for (int i = 0; i < count + 1; i++) {
-        // yVals1.add(new Entry((float) (Math.random() * mult) + mult / 5, i));
-        // }
-
+        yVals1.add(new Entry(mCheckInRate, 0));
+        yVals1.add(new Entry(1.0f - mCheckInRate, 1));
         ArrayList<String> xVals = new ArrayList<String>();
-
-        for (int i = 0; i < count + 1; i++)
-            xVals.add(mParties[i % mParties.length]);
+        for (int i = 0; i < 2; i++)
+            xVals.add(infoStrings[i % infoStrings.length]);
 
         PieDataSet dataSet = new PieDataSet(yVals1, " ");
         dataSet.setSliceSpace(3f);
         dataSet.setSelectionShift(5f);
-
-        // add a lot of colors
-
         ArrayList<Integer> colors = new ArrayList<Integer>();
-
         colors.add(Color.rgb(192, 255, 140));
         colors.add(Color.GRAY);
-        // for (int c : ColorTemplate.VORDIPLOM_COLORS)
-        // colors.add(c);
-
-        // for (int c : ColorTemplate.JOYFUL_COLORS)
-        // colors.add(c);
-        //
-        // for (int c : ColorTemplate.COLORFUL_COLORS)
-        // colors.add(c);
-        //
-        // for (int c : ColorTemplate.LIBERTY_COLORS)
-        // colors.add(c);
-        //
-        // for (int c : ColorTemplate.PASTEL_COLORS)
-        // colors.add(c);
-
         colors.add(ColorTemplate.getHoloBlue());
-
         dataSet.setColors(colors);
 
         PieData data = new PieData(xVals, dataSet);
         data.setValueFormatter(new PercentFormatter());
         data.setValueTextSize(11f);
         data.setValueTextColor(Color.BLUE);
-        mChart.setData(data);
-        // undo all highlights
-        mChart.highlightValues(null);
 
-        mChart.invalidate();
+        pieChart.setData(data);
+        pieChart.highlightValues(null);
+        pieChart.invalidate();
+    }
+
+    private void setDefaultPieChartStyle(PieChart pieChart) {
+        pieChart.setUsePercentValues(true);
+        pieChart.setDescription("");
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColorTransparent(true);
+        pieChart.setTransparentCircleColor(Color.WHITE);
+        pieChart.setHoleRadius(1f);
+        pieChart.setTransparentCircleRadius(2f);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setRotationAngle(0);
+        pieChart.setRotationEnabled(true);
+        pieChart.setVisibility(View.INVISIBLE);
+        Legend l = pieChart.getLegend();
+        l.setEnabled(false);
     }
 }
