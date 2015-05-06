a00398cdeb9333962ef7fba7a18f6b4053aff4b7
hitYOUcry <285692817@qq.com>
2015-05-06 18:27:20 +0800

    .

---
+++ kcb/src/com/kcb/teacher/fragment/EditTestFragment.java
@@ -0,0 +1,12 @@
+package com.kcb.teacher.fragment;
+
+import com.kcb.common.base.BaseFragment;
+
+public class EditTestFragment extends BaseFragment {
+
+    @Override
+    protected void initView() {}
+
+    @Override
+    protected void initData() {}
+}
