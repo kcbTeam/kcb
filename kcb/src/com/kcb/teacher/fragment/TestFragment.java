cde26522262234fc58b7713d107880a3da2232e0
hitYOUcry <285692817@qq.com>
2015-05-09 15:48:23 +0800

    originze imports

--- kcb/src/com/kcb/teacher/fragment/TestFragment.java
+++ kcb/src/com/kcb/teacher/fragment/TestFragment.java
@@ -2,7 +2,6 @@ package com.kcb.teacher.fragment;
 
 import java.util.ArrayList;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
