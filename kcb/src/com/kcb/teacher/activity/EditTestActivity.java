63288552a82cd32374f90f0c679e4bc886980036
hitYOUcry <285692817@qq.com>
2015-05-26 21:01:14 +0800

    edittest some obvious error

--- kcb/src/com/kcb/teacher/activity/EditTestActivity.java
+++ kcb/src/com/kcb/teacher/activity/EditTestActivity.java
@@ -386,13 +386,13 @@ public class EditTestActivity extends BaseActivity implements OnLongClickListene
                             break;
                         case CLICK_TAG_A:
                             choiceAEditText.setText("");
-                            choiceBEditText.setFocusable(false);
+                            choiceAEditText.setFocusable(false);
                             choiceAEditText.setBackground(new BitmapDrawable(bitmap));
                             getCurrentQuestion().getChoiceA().setBitmap(bitmap);
                             break;
                         case CLICK_TAG_B:
-                            choiceCEditText.setText("");
-                            choiceDEditText.setFocusable(false);
+                            choiceBEditText.setText("");
+                            choiceBEditText.setFocusable(false);
                             choiceBEditText.setBackground(new BitmapDrawable(bitmap));
                             getCurrentQuestion().getChoiceB().setBitmap(bitmap);
                             break;
@@ -403,7 +403,7 @@ public class EditTestActivity extends BaseActivity implements OnLongClickListene
                             getCurrentQuestion().getChoiceC().setBitmap(bitmap);
                             break;
                         case CLICK_TAG_D:
-                            choiceCEditText.setText("");
+                            choiceDEditText.setText("");
                             choiceDEditText.setFocusable(false);
                             choiceDEditText.setBackground(new BitmapDrawable(bitmap));
                             getCurrentQuestion().getChoiceD().setBitmap(bitmap);
