e58e994ffed6a5f7c1f38c1ec661e3f152aded2b
wanghang <847972062@qq.com>
2015-05-28 16:54:26 +0800

    fix modifypass word

--- kcb/src/com/kcb/student/activity/ModifyPasswordActivity.java
+++ kcb/src/com/kcb/student/activity/ModifyPasswordActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.RelativeLayout;
 
 import com.android.volley.Request.Method;
 import com.android.volley.Response.ErrorListener;
@@ -13,6 +12,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.kcb.common.application.KAccount;
 import com.kcb.common.base.BaseActivity;
+import com.kcb.common.listener.DelayClickListener;
 import com.kcb.common.server.RequestUtil;
 import com.kcb.common.server.ResponseUtil;
 import com.kcb.common.server.UrlUtil;
@@ -21,20 +21,26 @@ import com.kcb.common.util.ToastUtil;
 import com.kcb.library.view.FloatingEditText;
 import com.kcb.library.view.PaperButton;
 import com.kcb.library.view.buttonflat.ButtonFlat;
+import com.kcb.library.view.smoothprogressbar.SmoothProgressBar;
 import com.kcbTeam.R;
 
 public class ModifyPasswordActivity extends BaseActivity {
 
-    private static final String TAG = ModifyPasswordActivity.class.getName();
+    private final String TAG = ModifyPasswordActivity.class.getName();
 
     private ButtonFlat backButton;
-    private FloatingEditText userpasswordEditText;
+
+    private FloatingEditText oldPasswordEditText;
     private PaperButton nextButton;
-    private PaperButton completeButton;
-    private FloatingEditText newPassWord;
-    private FloatingEditText repeatNewPassWord;
-    private RelativeLayout mLayout;
-    private String newpassword;
+    private SmoothProgressBar nextProgressBar;
+
+    private View newPasswordLayout;
+    private FloatingEditText newPasswordEditText;
+    private FloatingEditText repeatPassWordEditText;
+    private PaperButton finishButton;
+    private SmoothProgressBar finishProgressBar;
+
+    private String newPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,22 +48,25 @@ public class ModifyPasswordActivity extends BaseActivity {
         setContentView(R.layout.tch_activity_modifypassword);
 
         initView();
+        initData();
     }
 
     @Override
     protected void initView() {
-
         backButton = (ButtonFlat) findViewById(R.id.button_back);
         backButton.setOnClickListener(this);
-        userpasswordEditText = (FloatingEditText) findViewById(R.id.edittext_oldpassword);
-        nextButton = (PaperButton) findViewById(R.id.button_next);
-        nextButton.setOnClickListener(this);
 
-        mLayout = (RelativeLayout) findViewById(R.id.layout_newpassword);
-        newPassWord = (FloatingEditText) findViewById(R.id.edittext_newpassword);
-        repeatNewPassWord = (FloatingEditText) findViewById(R.id.edittext_repeatpassword);
-        completeButton = (PaperButton) findViewById(R.id.button_complete);
-        completeButton.setOnClickListener(this);
+        oldPasswordEditText = (FloatingEditText) findViewById(R.id.edittext_oldpassword);
+        nextButton = (PaperButton) findViewById(R.id.button_next);
+        nextButton.setOnClickListener(mClickListener);
+        nextProgressBar = (SmoothProgressBar) findViewById(R.id.progressbar_next);
+
+        newPasswordLayout = findViewById(R.id.layout_newpassword);
+        newPasswordEditText = (FloatingEditText) findViewById(R.id.edittext_newpassword);
+        repeatPassWordEditText = (FloatingEditText) findViewById(R.id.edittext_repeatpassword);
+        finishButton = (PaperButton) findViewById(R.id.button_complete);
+        finishButton.setOnClickListener(mClickListener);
+        finishProgressBar = (SmoothProgressBar) findViewById(R.id.progressbar_finish);
     }
 
     @Override
@@ -69,78 +78,119 @@ public class ModifyPasswordActivity extends BaseActivity {
             case R.id.button_back:
                 finish();
                 break;
-            case R.id.button_next:
-                final String oldpassword = userpasswordEditText.getText().toString();
-                if (TextUtils.isEmpty(oldpassword)) {
-                    userpasswordEditText.requestFocus();
-                    AnimationUtil.shake(userpasswordEditText);
-                } else {
-                    StringRequest request =
-                            new StringRequest(Method.POST, UrlUtil.getStuCheckOldPasswordUrl(
-                                    KAccount.getAccountId(), oldpassword), new Listener<String>() {
-                                public void onResponse(final String response) {
-                                    new Handler().postDelayed(new Runnable() {
-
-                                        @Override
-                                        public void run() {
-                                            setNewPassWord();
-                                        }
-                                    }, 500);
-                                };
-                            }, new ErrorListener() {
-                                public void onErrorResponse(VolleyError error) {
-                                    if (null != error.networkResponse
-                                            && error.networkResponse.statusCode == 400) {
-                                        ToastUtil.toast(R.string.password_error);
-                                    } else {
-                                        ResponseUtil.toastError(error);
-                                    }
-                                };
-                            });
-                    RequestUtil.getInstance().addToRequestQueue(request, TAG);
-                }
-                break;
-            case R.id.button_complete:
-                if (comparaPassWord()) {
-                    StringRequest request =
-                            new StringRequest(Method.POST, UrlUtil.getStuModifyPasswordUrl(
-                                    KAccount.getAccountId(), newpassword), new Listener<String>() {
-                                public void onResponse(final String response) {
-                                    new Handler().postDelayed(new Runnable() {
-
-                                        @Override
-                                        public void run() {
-                                            ToastUtil.toast(R.string.modified);
-                                            finish();
-                                        }
-                                    }, 500);
-                                };
-                            }, new ErrorListener() {
-                                public void onErrorResponse(VolleyError error) {
-                                    ResponseUtil.toastError(error);
-                                };
-                            });
-                    RequestUtil.getInstance().addToRequestQueue(request, TAG);
-                }
-                break;
             default:
                 break;
         }
     }
 
-    private boolean comparaPassWord() {
-        if (!newPassWord.getText().toString().equals(repeatNewPassWord.getText().toString())) {
-            AnimationUtil.shake(newPassWord);
-            AnimationUtil.shake(repeatNewPassWord);
-            return false;
+    private DelayClickListener mClickListener = new DelayClickListener(
+            DelayClickListener.DELAY_PAPER_BUTTON) {
+
+        @Override
+        public void doClick(View v) {
+            switch (v.getId()) {
+                case R.id.button_next:
+                    if (oldPasswordEditText.isEnabled()) {
+                        checkOldPassword();
+                    }
+                    break;
+                case R.id.button_complete:
+                    setNewPassword();
+                    break;
+            }
         }
-        return true;
+    };
+
+    private void checkOldPassword() {
+        final String oldPassword = oldPasswordEditText.getText().toString();
+        if (TextUtils.isEmpty(oldPassword)) {
+            AnimationUtil.shake(oldPasswordEditText);
+        } else {
+            if (nextProgressBar.getVisibility() == View.VISIBLE) {
+                return;
+            }
+            nextProgressBar.setVisibility(View.VISIBLE);
+            StringRequest request =
+                    new StringRequest(Method.POST, UrlUtil.getStuCheckOldPasswordUrl(
+                            KAccount.getAccountId(), oldPassword), new Listener<String>() {
+
+                        @Override
+                        public void onResponse(String response) {
+                            new Handler().postDelayed(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    oldPasswordEditText.setEnabled(false);
+                                    nextProgressBar.setVisibility(View.GONE);
+                                    newPasswordLayout.setVisibility(View.VISIBLE);
+                                    AnimationUtil.fadeIn(newPasswordLayout);
+                                    newPasswordEditText.requestFocus();
+                                }
+                            }, 500);
+                        }
+                    }, new ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            nextProgressBar.hide(ModifyPasswordActivity.this);
+                            if (null != error.networkResponse
+                                    && error.networkResponse.statusCode == 400) {
+                                ToastUtil.toast(R.string.password_error);
+                            } else {
+                                ResponseUtil.toastError(error);
+                            }
+                        }
+                    });
+            RequestUtil.getInstance().addToRequestQueue(request, TAG);
+        }
     }
 
-    private void setNewPassWord() {
-        AnimationUtil.centerToLeft(userpasswordEditText);
-        AnimationUtil.centerToLeft(nextButton);
-        AnimationUtil.rightToCenter(mLayout);
-        mLayout.setVisibility(View.VISIBLE);
+    private void setNewPassword() {
+        String password = newPasswordEditText.getText().toString();
+        String repeatPassword = repeatPassWordEditText.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            AnimationUtil.shake(newPasswordEditText);
+            newPasswordEditText.requestFocus();
+        } else if (TextUtils.isEmpty(repeatPassword)) {
+            AnimationUtil.shake(repeatPassWordEditText);
+            repeatPassWordEditText.requestFocus();
+        } else if (!password.equals(repeatPassword)) {
+            ToastUtil.toast("两次输入的密码不一致");
+        } else {
+            if (finishProgressBar.getVisibility() == View.VISIBLE) {
+                return;
+            }
+            finishProgressBar.setVisibility(View.VISIBLE);
+            StringRequest request =
+                    new StringRequest(Method.POST, UrlUtil.getStuModifyPasswordUrl(
+                            KAccount.getAccountId(), newPassword), new Listener<String>() {
+
+                        @Override
+                        public void onResponse(String response) {
+                            new Handler().postDelayed(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    ToastUtil.toast(R.string.modified);
+                                    finish();
+                                }
+                            }, 500);
+                        }
+                    }, new ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            finishProgressBar.hide(ModifyPasswordActivity.this);
+                            ResponseUtil.toastError(error);
+                        }
+                    });
+            RequestUtil.getInstance().addToRequestQueue(request, TAG);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        RequestUtil.getInstance().cancelPendingRequests(TAG);
     }
 }
