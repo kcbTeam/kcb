9c80b91b14a793a0f9d4a1ab04f1166fced8e941
wanghang <847972062@qq.com>
2015-06-07 17:31:37 +0800

    add testview

---
+++ kcb/src/com/kcb/common/view/SearchEditText.java
@@ -0,0 +1,20 @@
+package com.kcb.common.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class SearchEditText extends RelativeLayout {
+
+    public SearchEditText(Context context) {
+        super(context);
+    }
+
+    public SearchEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SearchEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
